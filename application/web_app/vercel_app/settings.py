247fed05307de0808179f4588b1671798c4fb898
jlefevre559 <jlefevre559@gmail.com>
2025-05-06 23:17:22 -0600

    Add templates to DIRS

--- application/web_app/vercel_app/settings.py
+++ application/web_app/vercel_app/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = "vercel_app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["cap_ace_web/templates/"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
